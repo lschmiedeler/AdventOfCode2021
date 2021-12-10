61c3c8b75da9eb5b55c22cd1bdad0f4906b47d8c
Lauren Schmiedeler <leschmiedeler@gmail.com>
2021-12-09 21:50:43 -0600

    cleaned up day 9 puzzle 2

--- Day9/Day9Puzzle2.R
+++ Day9/Day9Puzzle2.R
@@ -1,5 +1,4 @@
 library(raster)
-library(tidyverse)
 
 input <- read.csv("Day9Input.txt", header = FALSE, colClasses = c("character"))
 input <- as.data.frame(do.call(rbind, strsplit(input[,1], split = "")),
@@ -11,6 +10,7 @@ input_vector[input_vector == 9] <- 0
 input <- matrix(input_vector, nrow = nrow(input), ncol = ncol(input))
 
 input_raster <- raster(input)
-freq_clumps <- sort(freq(clump(input_raster, directions = 4))[,2])
+freq_clumps <- freq(clump(input_raster, directions = 4))
+freq_clumps <- sort(freq_clumps[,2][!is.na(freq_clumps[,1])])
 n_clumps <- length(freq_clumps)
-freq_clumps[n_clumps - 3] * freq_clumps[n_clumps - 2] * freq_clumps[n_clumps - 1]
+freq_clumps[n_clumps - 2] * freq_clumps[n_clumps - 1] * freq_clumps[n_clumps]
