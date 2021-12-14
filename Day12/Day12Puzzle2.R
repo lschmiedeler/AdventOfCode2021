fcea3842aa7a8223551115bcfed0b60af67ec7d3
Lauren Schmiedeler <leschmiedeler@gmail.com>
2021-12-13 20:35:03 -0600

    last update to day 12

--- Day12/Day12Puzzle2.R
+++ Day12/Day12Puzzle2.R
@@ -13,27 +13,6 @@ input <- filter(rbind(input, as.data.frame(t(apply(input, MARGIN = 1, function(x
 all_nodes <- unique(as.vector(filter(input, V1 != "start"))$V1)
 lower_nodes <- all_nodes[all_nodes == tolower(all_nodes)]
 
-find_paths <- function(node, path) {
-  path <- c(path, node)
-  to_nodes <- filter(input, V1 == node)$V2
-  if ("end" %in% to_nodes & to_nodes[length(to_nodes)] != "end") {
-    i <- which(to_nodes == "end")
-    to_nodes <- c(to_nodes[-i], to_nodes[i])
-  }
-  for (to in to_nodes) {
-    if (to != "end") {
-      if (!(to %in% path) | to == toupper(to)) {
-        find_paths(to, path)
-      }
-    }
-    else {
-      path <- c()
-      n_paths <<- n_paths + 1
-      return(1)
-    }
-  }
-}
-
 find_paths <- function(node, path, repeat_lower) {
   path <- c(path, node)
   to_nodes <- filter(input, V1 == node)$V2
