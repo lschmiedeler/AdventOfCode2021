60b84fe76c7970e419cda465fa9aea20bad2f2ad
Lauren Schmiedeler <leschmiedeler@gmail.com>
2022-11-12 14:56:03 -0600

    minor formatting fixes to day 18 puzzles

--- Day18/Day18Puzzle1.py
+++ Day18/Day18Puzzle1.py
@@ -24,7 +24,6 @@ def explode(numbers_df):
     add1 = int(numbers_df.loc[row1,"value"])
     if row1 != 0:
         numbers_df.loc[row1-1,"value"] = add1 + int(numbers_df.loc[row1-1,"value"])
-
     row2 = rows[1]
     add2 = int(numbers_df.loc[row2,"value"])
     if row2 != len(numbers_df) - 1:
@@ -34,6 +33,15 @@ def explode(numbers_df):
     numbers_df.loc[row1,"depth"] = 3
     
     numbers_df = numbers_df.drop(numbers_df.index[row2])
+    return numbers_df.reset_index(drop = True)
+
+def explode_all(numbers_df):
+    continue_explode = True
+    while continue_explode:
+        if not numbers_df.query("depth >= 4").empty:
+            numbers_df = explode(numbers_df)
+        else:
+            continue_explode = False
     return numbers_df.reset_index(drop = True)
 
 def split(numbers_df):
@@ -56,24 +64,13 @@ def split(numbers_df):
 
 def find_magnitude(numbers_df):
     numbers_df = numbers_df.reset_index(drop = True)
-
     while len(numbers_df) > 1:
         rows = numbers_df[numbers_df.depth == numbers_df.depth.max()].head(2).index
         numbers_df.loc[rows[0],"depth"] = numbers_df.loc[rows[0], "depth"] - 1
         numbers_df.loc[rows[0],"value"] = 3 * numbers_df.loc[rows[0], "value"] + 2 * numbers_df.loc[rows[1], "value"]
         numbers_df = numbers_df.drop(numbers_df.index[rows[1]])
         numbers_df = numbers_df.reset_index(drop = True)
-    
     return numbers_df.loc[0,"value"]
-
-def explode_all(numbers_df):
-    continue_explode = True
-    while continue_explode:
-        if not numbers_df.query("depth >= 4").empty:
-            numbers_df = explode(numbers_df)
-        else:
-            continue_explode = False
-    return numbers_df.reset_index(drop = True)
 
 file = open("Day18Input.txt")
 lines = file.readlines()
